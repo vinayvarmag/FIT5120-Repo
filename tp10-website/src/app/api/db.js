1ed54710e5ecd4b6e6231d7f2ad4fd32cc3561f5
vinayvarmag <141786090+vinayvarmag@users.noreply.github.com>
2025-05-18 12:21:57 +1000

    Update db.js

--- tp10-website/src/app/api/db.js
+++ tp10-website/src/app/api/db.js
@@ -2,7 +2,7 @@ import mysql from "mysql2/promise";
 
 // In a real project, you’d keep these in .env.local or some safe place
 const pool = mysql.createPool({
-    host: "13.51.163.32",   // or your DB host
+    host: "13.60.174.225",   // or your DB host
     user: "remoteuser",        // your DB user
     password: "Admin123@",
     database: "TP10",  // name of the DB
